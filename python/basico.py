0e08851e89c4278452199279585daf50ac34e409
Suzana Barboza <phsuzana@gmail.com>
2019-01-08 19:30:26 -0200

    Sintaxe -
    Estudando livro Automate the boring stuff with python

---
+++ python/basico.py
@@ -0,0 +1,72 @@
+print('H3ll0 W0rld')
+
+# Operadores: 
+# ** -> (exponencial) 
+# % -> módulo 
+# // -> divisão inteira
+
+
+#Concatenação de String:
+# 'str1' + 'str2'
+
+#Replicação de String:
+#'str' * (int)
+print('Str' * 5)
+
+#Tamanho da string
+len('variavel')
+
+
+# Input:  input() - retorna sempre string
+answer = input('Digite uma palavra')
+print(answer)
+
+#Cast
+int('12')
+str(12)
+float(1)
+
+#Boolean
+True
+False
+
+#Boolean Operator
+and
+or 
+not
+
+#Condição
+if ():
+else:
+
+if ():
+elif():
+
+#Loop - pode usar break pra sair do loop
+while ():
+
+for i in range():
+
+#Range - conta de 2 em 1 - começa em 12 - para em 16
+range(12,16, 2)
+
+#Funções - retorna None se não definido
+def func(param1, param2):
+    return 'str'
+
+#Variável global dentro de uma função
+#Para utilizar basta fazer p seguinte
+def foo():
+    global variable
+    variable = 'value'
+
+variable = 'val'
+foo()
+
+# Geremciamento de Exceção
+try:
+except nome_do_erro:
+
+
+
+
